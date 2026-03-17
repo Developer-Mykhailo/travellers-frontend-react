2fb7f67b7d1d42cb67d9700bd8ce5b71420f84b2
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2026-03-16 18:41:29 -0600

    add Join component markup

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,9 +1,28 @@
-// import css from './Join.module.css'
+import css from './Join.module.css';
 
 import Container from '../Container/Container';
+import Section from '../Section/Section';
+import { Link } from 'react-router-dom';
+import ui from '../UI/ui.module.css';
+import clsx from 'clsx';
 
 const Join = () => {
-  return <Container>Join</Container>;
+  return (
+    <Section className={css.join}>
+      <Container>
+        <div className={css.content}>
+          <h2 className={css.title}>Join our growing community today</h2>
+          <p className={css.descr}>
+            Join a community of travelers who share their stories and inspire
+            unforgettable new adventures.
+          </p>
+          <Link className={clsx(ui.shared, ui.primary, css.signUp)}>
+            Sign up
+          </Link>
+        </div>
+      </Container>
+    </Section>
+  );
 };
 
 export default Join;
