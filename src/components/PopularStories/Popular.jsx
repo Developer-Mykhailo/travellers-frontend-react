8e6ad69942c6a3d75ff762a24289585da5b897b5
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2026-03-24 20:44:56 -0600

    add button

--- src/components/PopularStories/Popular.jsx
+++ src/components/PopularStories/Popular.jsx
@@ -1,14 +1,21 @@
+import { useMediaQuery } from 'react-responsive';
 import TravellersStoriesItem from '../../features/stories/components/TravellersStoriesItem/TravellersStoriesItem';
 import Container from '../common/Container/Container';
 import Section from '../Section/Section';
+import Button from '../UI/Button/Button';
 
 import css from './Popular.module.css';
 
 const Popular = () => {
+  const isTablet = useMediaQuery({ query: '(min-width: 768px)' });
+
+  // JSX
   return (
     <Section className={css.popularSection}>
       <Container>
         <TravellersStoriesItem />
+
+        {isTablet && <Button className={css.viewMoreBtn}>View more</Button>}
       </Container>
     </Section>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import About from '../../components/About/About';
-import Container from '../../components/common/Container/Container';
 import Hero from '../../components/Hero/Hero';
 import Join from '../../components/Join/Join';
 import OurTravellers from '../../components/OurTravellers/OurTravellers';
