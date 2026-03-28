aa5a66b84edbfaa1bb2a7b4849a717bde7b96e5d
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2026-03-28 16:28:30 -0600

    add styles; improve markup

--- src/pages/StoryPage/StoryPage.jsx
+++ src/pages/StoryPage/StoryPage.jsx
@@ -5,43 +5,47 @@ import placeholder from '../../assets/images/placeholder.jpg';
 import Button from '../../components/UI/Button/Button';
 
 import css from './StoryPage.module.css';
+import Popular from '../../components/PopularStories/Popular';
 
 const StoryPage = () => {
   const { owner, title, date, category, article, img } = response.data;
 
   // JSX
   return (
-    <Section className={css.storySection}>
-      <Container>
-        <h1 className={css.storyTitle}>{title}</h1>
+    <>
+      <Section className={css.storySection}>
+        <Container>
+          <h1 className={css.storyTitle}>{title}</h1>
+          <div className={css.storyData}>
+            <p className={css.nameAuthor}>
+              <span>Автор статті </span>
+              {owner}
+            </p>
+            <p className={css.published}>
+              <span>Опубліковано </span>
+              {date}
+            </p>
+            <span className={css.category}>{category}</span>
+          </div>
+          <div className={css.wrapImg}>
+            <img src={img || placeholder} alt="story picture" />
+          </div>
 
-        <div className={css.storyData}>
-          <p className={css.nameAuthor}>
-            <span>Автор статті </span>
-            {owner}
-          </p>
-          <p className={css.published}>
-            <span>Опубліковано </span>
-            {date}
-          </p>
-          <span className={css.category}>{category}</span>
-        </div>
+          <div className={css.contentBox}>
+            <p className={css.descr}>{article}</p>
+            <div className={css.saveStoryBox}>
+              <p className={css.saveStorytitle}>Keep the story </p>
+              <p className={css.saveStoryDescr}>
+                It will be available in your profile in the saved section
+              </p>
+              <Button className={css.saveStoryBtn}>Save</Button>
+            </div>
+          </div>
+        </Container>
+      </Section>
 
-        <div className={css.wrapImg}>
-          <img src={img || placeholder} alt="story picture" />
-        </div>
-
-        <p className={css.descr}>{article}</p>
-
-        <div className={css.saveStoryBox}>
-          <p className={css.saveStorytitle}>Keep the story </p>
-          <p className={css.saveStoryDescr}>
-            It will be available in your profile in the saved section
-          </p>
-          <Button className={css.saveStoryBtn}>Save</Button>
-        </div>
-      </Container>
-    </Section>
+      <Popular></Popular>
+    </>
   );
 };
 
