1934fa44e16cb9b87fcde94d49c18646347311da
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2026-06-11 13:59:40 -0600

    add userReducer

--- src/app/appStore.js
+++ src/app/appStore.js
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/store/slice';
 import publicStoriesReducer from '../features/stories/store/slice';
 import travellersReduser from '../features/travellers/store/slice';
+import userReducer from '../features/user/store/slice';
 
 const appStore = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
     publicStories: publicStoriesReducer,
     travellers: travellersReduser,
   },
