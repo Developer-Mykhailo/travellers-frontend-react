e6921d0257ee4aa5f39976c53202b695b0c70570
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2026-06-27 01:28:03 -0600

    change link

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -5,8 +5,11 @@ import Section from '../Section/Section';
 
 import ui from '../UI/ui.module.css';
 import css from './Join.module.css';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../features/auth/store/selectors';
 
 const Join = () => {
+  const isAuth = useSelector(selectIsAuth);
   return (
     <Section className={css.join} id="join">
       <Container>
@@ -17,10 +20,10 @@ const Join = () => {
             unforgettable new adventures.
           </p>
           <Link
-            to="/auth/login"
+            to={isAuth ? '/profile' : '/auth/register'}
             className={clsx(ui.shared, ui.primary, css.signUp)}
           >
-            Sign up
+            {isAuth ? 'Saved stories' : ' Sign up'}
           </Link>
         </div>
       </Container>
